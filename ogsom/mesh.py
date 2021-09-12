0f2708f35db427fb2e7f7e67762b23c52463c3c8
LaiQE <laiqe@zju.edu.cn>
2021-09-12 17:04:20 +0800

    add contact class

--- ogsom/mesh.py
+++ ogsom/mesh.py
@@ -2,12 +2,13 @@
 Description: In User Settings Edit
 Author: Qianen
 Date: 2021-09-11 16:54:20
-LastEditTime: 2021-09-11 17:00:13
+LastEditTime: 2021-09-12 16:33:31
 LastEditors: Qianen
 '''
 import os
 import trimesh
 import numpy as np
+from .face import Face
 
 
 class MeshBase(object):
@@ -17,8 +18,7 @@ class MeshBase(object):
     def __init__(self, trimesh_obj, name):
         '''
         trimesh_obj: 一个trimesh对象
-        name: 文件的名字
-        mode: 相交检测器类型, 可选'rtree'和'tvtk','tvtk'速度快但是不能多线程
+        name: 模型的名字
         '''
         # 一个trimesh对象,用来保存网格数据
         self.trimesh_obj = self.process_mesh(trimesh_obj)
@@ -29,21 +29,22 @@ class MeshBase(object):
         else:
             self.center_mass = self.trimesh_obj.centroid
 
-    def ray_test(self, ray_origins, ray_directions, max_distance=None):
+    def ray_test(self, ray_origin, ray_direction, max_distance=None):
         """ 使用rtree进行光线测试,最后的交点按照原点距离排序
             ray_origins: 光线原点, (3,)
             ray_directions: 光线方向, (3,)
         """
         points, _, ids = self.tria.intersects_location(np.reshape(
-            ray_origins, (1, 3)), np.reshape(ray_directions, (1, 3)))
+            ray_origin, (1, 3)), np.reshape(ray_direction, (1, 3)))
         if len(points) == 0:
             return np.array([]), np.array([])
         # 重新排序,使用rtree进行光线检查的结果顺序不是按照原点排序的
-        distance = np.linalg.norm(points - np.squeeze(ray_origins), axis=1)
+        distance = np.linalg.norm(points - np.squeeze(ray_origin), axis=1)
         order = np.argsort(distance)
         if max_distance is not None:
             order = [o for o in order if distance[o] <= max_distance]
-        return points[order], ids[order]
+        # TODO: 在边界上的点会被计算两次
+        return points[order], ids[order], distance[order]
 
     def intersect_line(self, lineP0, lineP1):
         """ 使用rtree进行相交检测, 直线的两个端点 """
@@ -88,3 +89,25 @@ class MeshBase(object):
                                       face_normals=normals,
                                       validate=True)
         return cls(trimesh_obj, name=name)
+
+
+class MeshFace(MeshBase):
+    def __init__(self, trimesh_obj, name):
+        super().__init__(trimesh_obj, name)
+        self.faces = self.get_faces(self.trimesh_obj)
+
+    @staticmethod
+    def get_faces(tri_obj):
+        v = tri_obj.vertices
+        n = tri_obj.face_normals
+        faces = []
+        for i, fv in enumerate(tri_obj.faces):
+            faces.append(Face(np.array(v[fv]), i, n[i]))
+        return faces
+
+    def get_face(self, face_id):
+        return self.faces[int(face_id)]
+
+    def find_other_contacts(self, c0):
+        # 找到其他的接触点
+        pass
